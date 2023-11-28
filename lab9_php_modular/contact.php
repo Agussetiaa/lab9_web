e3c5be61a48c5f2ff35cee1d43e956268b7d9336
AGUS SETIAWAN <115542822+Agussetiaa@users.noreply.github.com>
2023-11-28 21:44:18 +0700

    Add files via upload

---
+++ lab9_php_modular/contact.php
@@ -0,0 +1,11 @@
+<?php require('header.php'); ?>
+
+<div class="content">
+    <h2>Hubungi Kami</h2>
+    <?php
+    // Isi halaman kontak di sini, menggunakan koneksi database jika diperlukan
+    ?>
+    <p>Ini adalah bagian content dari halaman kontak.</p>
+</div>
+
+<?php require('footer.php'); ?>
